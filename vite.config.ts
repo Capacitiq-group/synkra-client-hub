ead2c41a450cb8b8ca8a96f244e04dac683abeac
Capacitiq Group <admin@capacitiqgroup.co.za>
2026-08-07 20:34:22 +0200

    Replace TanStackStartVite with tanstackStart

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { TanStackStartVite } from "@tanstack/react-start/plugin/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [
     tailwindcss(),
-    TanStackStartVite(),
+    tanstackStart(),
     react(),
     tsConfigPaths(),
   ],
